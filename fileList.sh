134986fc593d20b451609ff4626ae82bb93c6a32
songjiyang <2934238248@qq.com>
2024-12-26 14:33:25 +0800

    结构

--- fileList.sh
+++ fileList.sh
@@ -1,14 +1,14 @@
 #!/bin/sh 
 cat>data/video.txt<<EOF
-$(ls data -1 -R | grep mp4| while read line
+$(ls data/tennis/video -1 -R | grep mp4| while read line
 do 
-find -name $line &
+find data/tennis/video -name $line &
 done) 
 EOF
 cat>data/frame.txt<<EOF
-$(ls  -1 -R | grep png| while read line
+$(ls data/tennis/frame -1 -R | grep png| while read line
 do 
-find -name $line &
+find data/tennis/frame -name $line &
 done) 
 EOF
 
